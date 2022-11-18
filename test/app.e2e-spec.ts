9214e46dbd86f91ec02ed4afefddb441d80b0c71
Julian Lasso <98534497+ProfeJulianLasso@users.noreply.github.com>
2022-11-18 09:58:17 -0500

    Test funcionales e2e quedaron explicados e implementados

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
+import { DataSource } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  type MockType<T> = {
+    [P in keyof T]?: jest.Mock;
+  };
+  const dataSourceMockFactory: () => MockType<DataSource> = jest.fn(() => ({
+    createQueryRunner: jest.fn().mockImplementation(() => ({
+      connect: jest.fn(),
+      startTransaction: jest.fn(),
+      release: jest.fn(),
+      rollbackTransaction: jest.fn(),
+      manager: {
+        save: jest.fn().mockResolvedValue({
+          clienteNombre: 'Julian Lasso',
+          clienteCorreo: 'julian.lasso@sofka.com.co',
+          id: 1,
+        }),
+      },
+      commitTransaction: jest.fn(),
+    })),
+  }));
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      controllers: [AppController],
+      providers: [
+        AppService,
+        {
+          provide: DataSource,
+          useFactory: dataSourceMockFactory,
+        },
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -20,5 +48,20 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/ (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/')
+      .send({
+        clienteNombre: 'Julian Lasso',
+        clienteCorreo: 'julian.lasso@sofka.com.co',
+      })
+      .expect(201)
+      .expect({
+        clienteNombre: 'Julian Lasso',
+        clienteCorreo: 'julian.lasso@sofka.com.co',
+        id: 1,
+      });
   });
 });
